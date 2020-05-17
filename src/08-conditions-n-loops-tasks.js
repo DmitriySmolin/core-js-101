d1ea155b414582811912c990a54474ee1486ba19
Dmitriy Smolin <neox56@gmail.com>
2020-05-17 23:52:31 +0400

    feat: add solutions conditions-n-loops-tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -28,10 +28,12 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz( /* num */ ) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 !== 0) return 'Fizz';
+  if (num % 5 === 0 && num % 3 !== 0) return 'Buzz';
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  return num;
 }
-
 
 /**
  * Returns the factorial of the specified integer n.
@@ -44,10 +46,13 @@ function getFizzBuzz( /* num */ ) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial( /* n */ ) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let res = 1;
+  for (let i = 1; i <= n; i += 1) {
+    res *= i;
+  }
+  return res;
 }
-
 
 /**
  * Returns the sum of integer numbers between n1 and n2 (inclusive).
@@ -61,8 +66,15 @@ function getFactorial( /* n */ ) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers( /* n1, n2 */ ) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let sum = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    for (let k = 1; k < i; k += 1) {
+      if (i % k === 0 && k !== 1) break;
+    }
+    sum += i;
+  }
+  return sum;
 }
 
 
@@ -81,10 +93,10 @@ function getSumBetweenNumbers( /* n1, n2 */ ) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle( /* a, b, c */ ) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a < (b + c) && b < (a + c) && c < (a + b)) return true;
+  return false;
 }
-
 
 /**
  * Returns true, if two specified axis-aligned rectangles overlap, otherwise false.
@@ -149,8 +161,9 @@ function doRectanglesOverlap( /* rect1, rect2 */ ) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle( /* circle, point */ ) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const length = Math.sqrt((point.y - circle.center.y) ** 2 + (point.x - circle.center.x) ** 2);
+  return (length < circle.radius);
 }
 
 
@@ -165,8 +178,12 @@ function isInsideCircle( /* circle, point */ ) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar( /* str */ ) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  const arr = str.split('');
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    if (arr.filter((item) => item === arr[i]).length === 1) return arr[i];
+  }
+  return null;
 }
 
 
@@ -192,10 +209,16 @@ function findFirstSingleChar( /* str */ ) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString( /* a, b, isStartIncluded, isEndIncluded */ ) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let c1;
+  let c2;
+  if (isStartIncluded) c1 = '[';
+  else c1 = '(';
+  if (isEndIncluded) c2 = ']';
+  else c2 = ')';
+  if (a > b) return `${c1}${b}, ${a}${c2}`;
+  return `${c1}${a}, ${b}${c2}`;
 }
-
 
 /**
  * Reverse the specified string (put all chars in reverse order)
@@ -209,8 +232,8 @@ function getIntervalString( /* a, b, isStartIncluded, isEndIncluded */ ) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString( /* str */ ) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -226,8 +249,8 @@ function reverseString( /* str */ ) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger( /* num */ ) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return +num.toString().split('').reverse().join('');
 }
 
 
@@ -269,8 +292,13 @@ function isCreditCardNumber( /* ccn */ ) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot( /* num */ ) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = num;
+
+  while (sum > 9) {
+    sum = +sum.toString().split('').reduce((acc, item) => +acc + +item);
+  }
+  return sum;
 }
 
 
