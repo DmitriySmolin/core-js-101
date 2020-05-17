ac1ef8f82f45c6d17e04c59d75aa51366c0fa4ac
Dmitriy Smolin <neox56@gmail.com>
2020-05-17 23:55:24 +0400

    feat: update conditions-n-loops-tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -348,11 +348,16 @@ function isBracketsBalanced( /* str */ ) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString( /* num, n */ ) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  const arr = [];
+  let nn = num;
+  while (nn >= n) {
+    arr.push(nn % n);
+    nn = Math.trunc(nn / n);
+  }
+  arr.push(nn);
+  return +arr.reverse().join('');
 }
-
-
 /**
  * Returns the commom directory path for specified array of full filenames.
  *
